4afff3b0bcb0e0ce88812e13b341fd2430242bd5
amit_saluja@optum.com <asaluja1@LAMU02W626AHTD6.uhc.com>
2019-09-21 22:04:23 +0530

    cleanup

--- src/main/java/code/sample/app/rest/controller/UserController.java
+++ src/main/java/code/sample/app/rest/controller/UserController.java
@@ -1,7 +1,7 @@
 package code.sample.app.rest.controller;
 
 import java.util.HashMap;
-import java.util.LinkedList;
+
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import code.sample.app.rest.ui.models.User;
-
 @RestController("/user")
 public class UserController {
 	private AtomicLong userID = new AtomicLong(0l);
@@ -31,7 +29,6 @@ public class UserController {
 		Long id = userID.incrementAndGet();
 		user.setId(id);
 		userList.put(id,user);
-		System.out.println(userList);
 		return user;
 
 	}
@@ -44,7 +41,6 @@ public class UserController {
 	 @GetMapping(value="/user/{userid}")
 	 @ResponseBody
      public User getUser (@PathVariable(value= "userid") Long userID) {
-		System.out.println("getting get ..."+userID+"    "+userList.get(userID));
 		return  userList.get(userID);
 	}
 	 
